659c75a407838005dd10becee0feb06edc4961ce
Luciana Fujii Pontello <luciana@fujii.eti.br>
2010-01-13 16:42:20 -0200

    Program to build pipeline for facetracker

---
+++ trackerlaunch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import gobject
+import pygst
+pygst.require("0.10")
+import gst
+
+class Gst_New_Launch:
+
+	def __init__ (self):
+
+		self.player = gst.Pipeline("player") 
+		source = gst.element_factory_make("v4l2src", "source") 
+		decoder = gst.element_factory_make("ycbcrenc", "decoder") 
+		conv = gst.element_factory_make("facetracker", "converter") 
+		encoder = gst.element_factory_make("ycbcrdec", "encoder") 
+		sink = gst.element_factory_make("xvimagesink", "sink") 
+		self.player.add(source, decoder, conv, encoder, sink) 
+		gst.element_link_many(source, decoder, conv, encoder, sink) 
+		
+		self.player.set_state(gst.STATE_PLAYING) 
+
+		bus = self.player.get_bus() 
+		bus.add_signal_watch() 
+		bus.connect("message", self.on_message) 
+
+	def on_message(self, bus, message): 
+		t = message.type 
+		if t == gst.MESSAGE_EOS: 
+			self.player.set_state(gst.STATE_NULL) 
+		elif t == gst.MESSAGE_ERROR: 
+			self.player.set_state(gst.STATE_NULL) 
+			loop.quit()
+
+Gst_New_Launch()
+loop = gobject.MainLoop()
+loop.run()
